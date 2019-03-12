8e858626c9b3156d91ba96720b55313f81a2681c
zhouwei <1695209838@qq.com>
2019-03-12 11:48:56 +0800

    Springboot  错误处理

---
+++ demo5/src/main/java/com/fabu/demo5/Demo5Application.java
@@ -0,0 +1,18 @@
+package com.fabu.demo5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*****
+ * 启动类  main
+ */
+@SpringBootApplication
+public class Demo5Application {
+
+	public static void main(String[] args) {
+
+	    SpringApplication.run(Demo5Application.class, args);
+	}
+
+}
+
